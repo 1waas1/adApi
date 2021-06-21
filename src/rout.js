35242f93be02ed4288ca543389a312c5e7ca0ae8
1waas1 <yAROSLAW1999>
2021-06-21 20:04:39 +0300

    update

--- src/rout.js
+++ src/rout.js
@@ -20,7 +20,9 @@ app.get('/ads-list/:page', async (req, res) => {
     let adsList = await prepareData.getPaginationListAds(page, req.query?.field,  req.query?.params)
 
     if (adsList === null){
-        res.send({'error':'Page not found'})
+        res.status(404)
+            .type('text/html')
+            .send('<h3>Page not found</h3>');
     }
     else {
         res.render('list',
@@ -33,6 +35,20 @@ app.get('/ads-list/:page', async (req, res) => {
                 'paramSort': information.paramSort,
             })
     }
+});
+
+app.get('/ads/item/:id', async (req, res) => {
+    let ad = await prepareData.getItemAds(parseInt(req.params.id))
+
+    if (ad === null){
+        res.status(404)
+            .type('text/html')
+            .send('<h3>Entries not found</h3>')
+    }
+    else {
+        res.render('adsItem', ad)
+    }
+
 });
 
 app.get('/list', async (req, res) => {
@@ -49,20 +65,6 @@ app.get('/list', async (req, res) => {
             .type('application/json')
             .json(allAdsList)
     }
-});
-
-app.get('/ads/item/:id', async (req, res) => {
-    let ad = await prepareData.getItemAds(parseInt(req.params.id))
-
-    if (ad === null){
-        res.status(404)
-            .type('text/html')
-            .send('<h3>Entries not found</h3>')
-    }
-    else {
-        res.render('adsItem', ad)
-    }
-
 });
 
 app.get('/ads/:id', async (req, res) => {
